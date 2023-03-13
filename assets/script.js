4fcc4f730cb0610b7b9822337b9e969106c6ce22
C12 <mcpartlandpatrick@gmail.com>
2023-03-13 16:20:01 -0400

    Added JS to study guide

--- assets/script.js
+++ assets/script.js
@@ -39,6 +39,6 @@ console.log("Here are the topics we learned through Prework:");
 
 listTopics()
 
-console.log("What would you like to study first?");
+console.log("Let's study this topic first!");
 
 selectTopic()
